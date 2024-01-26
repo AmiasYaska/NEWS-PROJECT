69357ae1ee174adad840a934e745b197b12f88f1
Amias Yaska <amiasyaska@gmail.com>
2024-01-26 22:52:00 +0300

    scrapes titles of first page only

---
+++ tv_west.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+import time
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+url = 'https://www.tvwest.co.ug/'
+driver.get(url)
+
+time.sleep(2)
+
+data = []  # Store all scraped data in a list
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    try:
+        new_height = WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, ".active .item .post-title a"))
+        ).location["y"]
+
+    except:
+        break
+
+    if new_height == last_height:
+        break
+    else:
+        last_height = new_height
+
+    # Extract text from all selectors and append to data list
+    for titles in [(".active .item .post-title", "Top stories title"), ("#home .post-title", "Recent news title"),
+                   (".mb-20 .post-title", "Ebyemizaano title")]:
+        selector, key = titles  # Unpack the tuple
+
+        for title_element in driver.find_elements(By.CSS_SELECTOR, selector):  # Pass selector separately
+            data.append({key: title_element.text})
+
+with open('tv_west_news.json', 'w') as f:
+    json.dump(data, f, indent=4)  # Indent for readability
+
+print("Data saved to tv_west_news.json")
+
+driver.quit()
